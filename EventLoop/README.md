8406b468451364c92f70a3b4bcd5e904d0ad16d3
Alan <alantee0302@gmail.com>
2023-09-26 16:22:25 +0800

    prototype

---
+++ EventLoop/README.md
@@ -0,0 +1,4 @@
+# Event Loop（事件循环）
+
+### What is Process？ （什么是进程？）
+
---
+++ Question/question.js
@@ -0,0 +1,18 @@
+// Question 1 - prototype
+
+// How to get the contrusted function name that create "obj"?
+function A(){}
+function B(){}
+
+function create(){
+    if(Math.random()<0.5){
+        return new A()
+    }else {
+        return new B()
+    }
+}
+
+var obj = create()
+
+// Ans: 
+console.log(obj, "=", obj.__proto__.constructor.name)
